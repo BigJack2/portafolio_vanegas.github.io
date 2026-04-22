fe022c4d8e143ba389e1b47cbc29c8d11be329cf
“BigJack2” <jsvanegas2014@gmail.com>
2026-04-22 10:51:48 -0500

    Se aplican correcciones variadas.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,3 @@
-function calcularEdad(anioNacimiento) {
-  const anioActual = new Date().getFullYear();
-  return anioActual - anioNacimiento;
-}
-
-// Establece el año de nacimiento y calcula la edad
-const anioNacimiento = 1993;
-const edad = calcularEdad(anioNacimiento);
-
-// Inserta la edad en el elemento HTML con id "edad"
-document.getElementById('edad').textContent = edad;
-
-
-
 // Diccionario de traducciones (natural y fluido)
 const TRANSLATIONS = {
   es: {
